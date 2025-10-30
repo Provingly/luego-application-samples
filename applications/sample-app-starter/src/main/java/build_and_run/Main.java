a59dfcc3808c6e0385d7a810ecfd0a30be83f142
Jean-Christophe Jardinier <jcjardinier@gmail.com>
2025-10-30 22:48:56 +0100

    Prepare call to the Luego compiler CLI from Java

--- applications/sample-app-starter/src/main/java/build_and_run/Main.java
+++ applications/sample-app-starter/src/main/java/build_and_run/Main.java
@@ -2,13 +2,7 @@ package build_and_run;
 
 public class Main {
   static void main(String[] args) {
-    System.out.println("Building and running a Luego app");
-
-    String name = "Joe";
-    String message = """
-            Hello %s
-            This is a multiline string.
-            """.formatted(name);
-    System.out.println(message);
+    System.out.println("Calling the Luego compiler CLI from Java");
+    luego.CLI.main(args);
   }
 }
