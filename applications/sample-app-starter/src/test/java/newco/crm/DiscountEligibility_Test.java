7847e91989e5512b4791aef7298d7855ff453a0d
Jean-Christophe Jardinier <jcjardinier@gmail.com>
2025-11-04 22:01:18 +0100

    Fix unit tests

--- applications/sample-app-starter/src/test/java/newco/crm/DiscountEligibility_Test.java
+++ applications/sample-app-starter/src/test/java/newco/crm/DiscountEligibility_Test.java
@@ -3,21 +3,14 @@ package newco.crm;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import luego.build.application.LuegoAppBuilder;
 import luego.runtime.application.LuegoAppInfo;
 import luego.runtime.application.LuegoRunner;
-import luego.runtime.results.Known;
 import luego.runtime.results.Result;
 import luego.runtime.values.PreEvaluationError;
 import luego.types.LGType;
-import os.Path;
 import scala.Tuple2;
 import scala.util.Either;
 
@@ -29,14 +22,8 @@ class DiscountEligibility_Test {
     static void setup() {
         System.out.println("@BeforeAll - executes once before all test methods in this class");
 
-        java.nio.file.Path applicationPathNio = Paths.get(".").toAbsolutePath();
-        
-        Path applicationPath = new Path(applicationPathNio);
-        LuegoAppInfo appInfo = LuegoAppBuilder.getLuegoAppInfo(applicationPath);
-
-        java.nio.file.Path applicationRuntimePathNio = Paths.get("./target/luego/app/" + appInfo.appId() + "/" + appInfo.appVersion()).toAbsolutePath();
-        Path applicationRuntimePath = new Path(applicationRuntimePathNio);
-        appRunner = new LuegoRunner(applicationRuntimePath);
+        LuegoAppInfo appInfo = LuegoAppInfo.read(".");
+        appRunner = LuegoRunner.apply("./target/luego/app/" + appInfo.appId() + "/" + appInfo.appVersion());
     }  
 
 
